c7cea1e9b617d5e5b5deef1fdf5574a7b4f40a90
Austin Ayers <ayers111@mail.chapman.edu>
2016-03-18 07:10:53 +0000

    Added py files

---
+++ Unstable_ODE.py
@@ -0,0 +1,14 @@
+#! /usr/bin/env python
+
+"""
+File: Unstable_ODE.py
+Copyright (c) 2016 Austin Ayers
+License: MIT
+
+Course: PHYS227
+Assignment: C.4 *BOOM*
+Date: March 18
+Email: ayers111@mail.chapman.edu
+Name: Austin Ayers
+Description: Oscillatory behavior in an ODE
+"""
---
+++ yx ODE.py
@@ -0,0 +1,14 @@
+#! /usr/bin/env python
+
+"""
+File: yx ODE.py
+Copyright (c) 2016 Austin Ayers
+License: MIT
+
+Course: PHYS227
+Assignment: C.3
+Date: March 17
+Email: ayers111@mail.chapman.edu
+Name: Austin Ayers
+Description: Solves an ODE problem using the forward Euler method
+"""
